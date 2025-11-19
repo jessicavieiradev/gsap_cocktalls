e166cd5015a4f4e98696090a8ea7a9d543b4695e
“jessicavieiradev” <misspandaicebear@gmail.com>
2025-11-19 11:55:41 -0300

    implement the first part of the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
+  );
 }
 
 export default App;
